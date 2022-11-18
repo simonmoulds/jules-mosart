ac9cc324b3c04f25ed3b29dc801abe19983d7b84
simonmoulds <sim.moulds@gmail.com>
2022-11-18 15:43:00 +0000

    try tempfile again

--- workflow/scripts/resample-jules-output.py
+++ workflow/scripts/resample-jules-output.py
@@ -75,20 +75,21 @@ def main(inputfile, outputfile, config):
         tmp_file = tempfile.NamedTemporaryFile(suffix='.nc')
         nc_outputfile = os.path.join(path, basename + '.regrid.nc')
         # Use bilinear interpolation for all continuous variables
+        tmp_file = tempfile.NamedTemporaryFile(suffix='.nc')
         subprocess.run([
             'cdo',
             'remapbil,' + GRIDFILE,
             filepath,
-            os.path.join(TMPDIR, 'tmp.nc')
-            # tmp_file.name
+            # os.path.join(TMPDIR, 'tmp.nc')
+            tmp_file.name
         ])
         # Ensure that variables have datatype 'double', not 'short',
         # which seems to cause problems in JULES (not exactly sure why...)
         subprocess.run([
             'cdo',
             '-b', 'F64', 'copy',
-            os.path.join(TMPDIR, 'tmp.nc'),
-            # tmp_file.name,
+            # os.path.join(TMPDIR, 'tmp.nc'),
+            tmp_file.name,
             nc_outputfile
         ])
         output_filelist.write(("%s" + os.linesep) % nc_outputfile)
