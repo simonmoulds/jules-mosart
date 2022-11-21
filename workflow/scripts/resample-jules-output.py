97afbb03d63addb3b974a6adf595719862416a86
simonmoulds <sim.moulds@gmail.com>
2022-11-21 12:16:12 +0000

    small fix

--- workflow/scripts/resample-jules-output.py
+++ workflow/scripts/resample-jules-output.py
@@ -98,7 +98,7 @@ def main(inputfile, outputfile, config):
         output_filelist.write(("%s" + os.linesep) % nc_outputfile)
         # Tidy up
         # os.remove(os.path.join(TMPDIR, 'tmp.nc'))
-        os.remove(tmp_file.name)
+        os.remove(tmp_file) #.name)
 
     output_filelist.close()
         
